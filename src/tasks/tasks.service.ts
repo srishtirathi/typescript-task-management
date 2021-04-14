fba81f188299168c2c0071c55e722222e2b0daff
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-04-14 06:49:32 +0530

    feat: create a new task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, taskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,16 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      title,
+      description,
+      id: uuid(),
+      status: taskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
